102d3a84b0d6a03a32e4ae0c586d374445418de1
Nehra <deepaknehra416@gmail.com>
2024-09-16 13:08:35 +0530

    first commit

---
+++ src/componets/CountrySearch.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CountrySearch = () => {
+  return (
+    <div>CountrySearch</div>
+  )
+}
+
+export default CountrySearch
---
+++ src/componets/ErrorNotification.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ErrorNotification = () => {
+  return (
+    <div>ErrorNotification</div>
+  )
+}
+
+export default ErrorNotification
